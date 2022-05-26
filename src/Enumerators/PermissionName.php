76079b24ccace52956f6fc7bad903a967bf51fcf
stephenjude <stephenjudesuccess@gmail.com>
2022-05-26 16:29:21 +0100

    remove final modifier from constants to allow usage on PHP 8.0

--- src/Enumerators/PermissionName.php
+++ src/Enumerators/PermissionName.php
@@ -5,8 +5,8 @@ namespace Chiiya\FilamentAccessControl\Enumerators;
 class PermissionName
 {
     /** @var string */
-    final public const UPDATE_ADMIN_USERS = 'admin-users.update';
+    public const UPDATE_ADMIN_USERS = 'admin-users.update';
 
     /** @var string */
-    final public const UPDATE_PERMISSIONS = 'permissions.update';
+    public const UPDATE_PERMISSIONS = 'permissions.update';
 }
--- src/Enumerators/RoleName.php
+++ src/Enumerators/RoleName.php
@@ -5,5 +5,5 @@ namespace Chiiya\FilamentAccessControl\Enumerators;
 class RoleName
 {
     /** @var string */
-    final public const SUPER_ADMIN = 'super-admin';
+    public const SUPER_ADMIN = 'super-admin';
 }
