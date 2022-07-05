e26747165999d2a13320aa0a518ecfe933614700
stephenjude <stephenjudesuccess@gmail.com>
2022-07-05 03:07:17 +0100

    Fixed Column not found when updating roles permissions

--- src/Resources/RoleResource.php
+++ src/Resources/RoleResource.php
@@ -29,7 +29,7 @@ class RoleResource extends Resource
                     ->required()
                     ->maxLength(255)
                     ->unique(
-                        config('permission.table_names.permissions'),
+                        config('permission.table_names.roles'),
                         'name',
                         fn (?Role $record): ?Role => $record,
                     ),
