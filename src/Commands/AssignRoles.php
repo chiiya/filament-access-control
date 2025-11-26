016ffc922c83ec5e44020be0c5a850c0129a8f11
Andreas Schneider <schneider@mindtwo.de>
2025-11-26 08:45:11 +0100

    fix: typo fix: remove resetting of guard

--- src/Commands/AssignRoles.php
+++ src/Commands/AssignRoles.php
@@ -5,6 +5,7 @@ namespace Chiiya\FilamentAccessControl\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role;
 
 use function Laravel\Prompts\search;
 
@@ -15,7 +16,7 @@ class AssignRoles extends Command implements PromptsForMissingInput
      *
      * @var string
      */
-    protected $signature = 'filament-access-control:assgin-roles {user} {role}';
+    protected $signature = 'filament-access-control:assign-roles {user} {role}';
 
     /**
      * The console command description.
@@ -52,11 +53,7 @@ class AssignRoles extends Command implements PromptsForMissingInput
             return self::FAILURE;
         }
 
-        // Set default auth guard for spatie to find the roles
-        $guardName = config('filament-access-control.guard_name', 'filament');
-        config()->set('auth.defaults.guard', $guardName);
-
-        $role = $this->argument('role');
+        $role = Role::findByName($this->argument('role'), config('filament-access-control.guard_name', 'filament'));
 
         $user->assignRole($role);
         $this->info("The role {$role} was assigned to User {$user->id}.");
@@ -73,15 +70,14 @@ class AssignRoles extends Command implements PromptsForMissingInput
     {
         return [
             'user' => fn () => search(
-                label: 'Search for a user:',
-                placeholder: 'E.g. Administrator',
+                label: 'Search for a user by email:',
                 options: fn ($value) => mb_strlen($value) > 0
-                    ? static::getUserModel()::whereLike('name', "%{$value}%")->pluck('name', 'id')->all()
+                    ? static::getUserModel()::whereLike('email', "%{$value}%")->pluck('email', 'id')->all()
                     : [],
             ), 'Which user ID do you want to manage?',
             'role' => fn () => search(
                 label: 'Search for a role:',
-                placeholder: 'E.g. admin',
+                placeholder: 'e.g. admin',
                 options: fn ($value) => mb_strlen($value) > 0
                     ? static::getRoleModel()::whereLike('name', "%{$value}%")->where('guard_name', 'filament')->pluck('name')->all()
                     : [],
